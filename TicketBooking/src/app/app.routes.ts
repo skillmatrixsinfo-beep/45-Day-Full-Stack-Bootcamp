d760fd0855e823b67cbbcadd4a96b97f21fc6578
Avinash Konduri <konduriavinashbala@gmail.com>
2025-12-28 16:21:11 +0530

    Initialiaste Ui Applications

---
+++ TicketBooking/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+
+export const routes: Routes = [
+    {path: 'dashboard', component:Dashboard},
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
+];
